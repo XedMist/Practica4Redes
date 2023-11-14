a055733766a43ed47a6c1827ab12d0b864c64116
XedMist <miguelangeliglesiasgarcia04@gmail.com>
2023-11-14 13:01:19 +0100

    Comentarios2

--- p1o.c
+++ p1o.c
@@ -4,23 +4,38 @@
 #include <unistd.h>
 #include <math.h>
 #include <sys/wait.h>
+
 #define ITERACIONES 10e9
 #define NPROCESOS 16
+#define LNOMBRE 30
+//Funcion auxiliar para calcular el tiempo
 float time_diff(struct timeval inicio, struct timeval final){
     return (final.tv_sec - inicio.tv_sec +(final.tv_usec - inicio.tv_usec) / 1.e6) * 1.e3;
 }
 int main(){
+    //Reservamos memoria para los nombres de los archivos
     char ** nombres = malloc(sizeof(char*)*NPROCESOS);
+    //Estructuras para medir el tiempo
     struct timeval inicio,final;
+    //Creamos los nombres de los archivos
+    //Cada proceso tendra un nombre diferente con el formato P<pid>-<numero>
     for(int i = 0;i <NPROCESOS;i++){
-        char * nombre = malloc(sizeof(char)*30);
+        //Reservamos memoria para el nombre
+        char * nombre = malloc(sizeof(char)*LNOMBRE);
         sprintf(nombre,"P%d-%d",getpid(),i);
         nombres[i] = nombre;
     }
+    //Inicio de la medicion de tiempo
     gettimeofday(&inicio, NULL);
+    //Creamos los procesos
     for(int i = 0; i < NPROCESOS; i++){
         if(fork() == 0){
+            //Cada proceso calcula su parte y la guarda en un archivo
+
+            //Abrimos el archivo
             FILE *f = fopen(nombres[i],"w");
+            
+            //Realizamos los calculos
             double ciclos = ITERACIONES/(NPROCESOS*10);
             long double resultadoParcial = 0;
             for(unsigned long j = 0;j < ciclos;j++){
@@ -29,16 +44,20 @@ int main(){
                 }
 
             }
+            //Guardamos el resultado en el archivo
             fprintf(f,"%.19Lf\n",resultadoParcial);
             fclose(f);
             exit(EXIT_SUCCESS);
         }
     }
+    //Esperamos a que terminen los procesos
     for(int i = 0;i < NPROCESOS;i++){
         wait(NULL);
     }
+
     long double sumaProcesos = 0;
     FILE *f;
+    //Leemos los archivos y sumamos los resultados
     for(int i = 0;i < NPROCESOS;i++){
         long double resultadoParcial;
         if((f = fopen(nombres[i],"r")) == NULL){
@@ -52,16 +71,22 @@ int main(){
         }
         sumaProcesos += resultadoParcial;
         fclose(f);
+        //Borramos el archivo
         if(remove(nombres[i]) != 0){
             perror("Error");
             exit(EXIT_FAILURE);
         }
+        //Liberamos la memoria del nombre
         free(nombres[i]);
     }
+    //Fin de la medicion de tiempo
     gettimeofday(&final, NULL);
+    //Calculamos el tiempo
     float tiempoProcesos = time_diff(inicio, final);
     printf("Los procesos han tardado %.16f ms y el resultado es %.19Lf\n", tiempoProcesos,sumaProcesos);
     free(nombres);
+    
+    //Calculamos el resultado de forma secuencial
     gettimeofday(&inicio, NULL);
     long double sumaMain = 0;
     for(unsigned long i = 0;i < ITERACIONES;i++){
@@ -71,7 +96,8 @@ int main(){
     float tiempoMain = time_diff(inicio, final);
     printf("El proceso principal ha tardado %.16f ms y el resultado es %.19Lf\n", tiempoMain,sumaMain);
     printf("La diferencia es %.19Lf\n",fabsl(sumaMain-sumaProcesos));
-
+    
+    //Comparamos los tiempos
     if(tiempoMain > tiempoProcesos){
         printf("Los procesos se han ahorrado %.16f ms con respecto al proceso principal\n",tiempoMain-tiempoProcesos);
     }else if(tiempoMain < tiempoProcesos){
